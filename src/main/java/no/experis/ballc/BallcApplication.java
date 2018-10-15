31a2e5db4bd700ef604fcf7e689c7cc755fafd26
Kenneth Storli <kenneth.storli@gmail.com>
2018-10-15 13:12:58 +0200

    Initial commit

---
+++ src/main/java/no/experis/ballc/BallcApplication.java
@@ -0,0 +1,12 @@
+package no.experis.ballc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallcApplication.class, args);
+	}
+}
