fb24c319311b150112acad4214895490952ea865
Rudi A Dahle <dahle05@gmail.com>
2018-10-18 10:15:54 +0200

    working on heroku with aws db

--- src/main/java/no/experis/ballc/repository/MatchJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package no.experis.ballc.repository;
-
-import no.experis.ballc.model.Match;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MatchJpaRepository extends JpaRepository<Match, Integer> {
-}
--- src/main/java/no/experis/ballc/repository/SeasonJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package no.experis.ballc.repository;
-
-import no.experis.ballc.model.Season;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SeasonJpaRepository extends JpaRepository<Season, Integer> {
-}
